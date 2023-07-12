c0c054c2824f20dac93e0a49d1b0c08458c962af
MOHAMMED SHEHSIN TA <mohammedshehsin654@gmail.com>
2023-07-12 10:01:45 +0200

    delected

--- AdvVariable/src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var a : String? = "abc"  // '?' it is used for null safety.
-    a = null
-    var nullLength = a?.length   //null safety.
-    println(a)
-    println("Null length is : $nullLength .")
-}
--- Switchnumber/src/main/kotlin/Main.kt
+++
@@ -1,6 +0,0 @@
-
-    fun main() {
-        val num = 254
-        val reversedNum = num.toString().reversed().toInt()
-        print(reversedNum)
-    }
